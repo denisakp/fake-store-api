4b5b635dde528d7a4e74e29be87591d7ac17cce0
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-06-15 21:40:40 +0100

    fixed connection to client

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,7 @@
-import { MongoClient, Db, ServerApiVersion } from 'mongodb';
-
-declare global {
-    var _mongoClientPromise: Promise<MongoClient>;
-}
+import {Db, MongoClient, ServerApiVersion} from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
-    throw new Error('Invalid environment variable: "MONGODB_URI"')
+    throw new Error('Please add your Mongo URI to .env')
 }
 
 const uri = process.env.MONGODB_URI
@@ -18,30 +14,27 @@ const options = {
 }
 
 let client
-let clientPromise: Promise<MongoClient>
+let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-    throw new Error('Please add your Mongo URI to .env.local')
-}
-
-// In development mode, use a global variable so that the value
-// is preserved across module reloads caused by HMR (Hot Module Replacement).
-// In production mode, it's best to not use a global variable.
 if (process.env.NODE_ENV === 'development') {
-    if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options)
-        global._mongoClientPromise = client.connect()
+    // In development mode, use a global variable so that the value
+    // is preserved across module reloads caused by HMR (Hot Module Replacement).
+    let globalWithMongo = global as typeof globalThis & {
+        _mongoClientPromise?: Promise<MongoClient>
     }
-    clientPromise = global._mongoClientPromise
+
+    if (!globalWithMongo._mongoClientPromise) {
+        client = new MongoClient(uri, options);
+        globalWithMongo._mongoClientPromise = client.connect();
+    }
+    clientPromise = globalWithMongo._mongoClientPromise;
 } else {
+    // In production mode, it's best to not use a global variable.
     client = new MongoClient(uri, options)
     clientPromise = client.connect()
 }
 
-
 export default async function connectToDB() {
     const client: MongoClient = await clientPromise;
-    const db: Db = client.db("store");
-
-    return {client, db};
+    return client.db("store")
 }
