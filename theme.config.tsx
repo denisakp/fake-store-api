6c22f03af73a4cef869d24a23ab8287c9495c131
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-08-21 16:20:11 +0100

    fixed SEO, and added logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,74 @@
-import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import {useRouter} from "next/router";
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-    logo: <span>Fake store API</span>,
+    logo: () => (
+        <>
+            <img src="/logo.png" alt="logo" style={{ height: "55px"}}/>
+
+            <span className="nx-ml-2 nx-text-2xl nx-font-bold"> Fake Store API</span>
+        </>
+    ),
     project: {
-        link: 'https://github.com/shuding/nextra-docs-template',
+        link: 'https://github.com/denisakp/fake-store-api',
     },
     chat: {
-        link: 'https://discord.com',
+        link: 'https://github.com/denisakp/fake-store-api/discussions',
     },
-    docsRepositoryBase: 'https://github.com/denisakp/nextra-docs-template',
+    docsRepositoryBase: 'https://github.com/denisakp/fake-store-api',
     footer: {
-        text: 'Nextra Docs Template',
+        text: `Fake store API © ${new Date().getUTCFullYear()} - Denis AKPAGNONITE`,
+    },
+    sidebar: {
+        defaultMenuCollapseLevel: 1,
+        toggleButton: true
+    },
+    primaryHue: 275,
+    useNextSeoProps() {
+        const { asPath } = useRouter()
+        if (asPath !== '/') {
+            return {
+                titleTemplate: '%s – Fake store API'
+            }
+        }
+    },
+    head: function useHead() {
+        const { title } = useConfig()
+        const { route } = useRouter()
+        const socialCard =
+            route === '/' || !title
+                ? 'https://nextra.site/og.jpeg'
+                : `https://nextra.site/api/og?title=${title}`
+
+        return (
+            <>
+                <meta name="msapplication-TileColor" content="#fff" />
+                <meta name="theme-color" content="#fff" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta httpEquiv="Content-Language" content="en" />
+                <meta
+                    name="description"
+                    content="Rapid E-commerce Prototyping Made Easy for developers"
+                />
+                <meta
+                    name="og:description"
+                    content="Rapid E-commerce Prototyping Made Easy for developers"
+                />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:image" content={socialCard} />
+                <meta name="twitter:site:domain" content="denisakp.me" />
+                <meta name="twitter:url" content="https://denisakp.me" />
+                <meta
+                    name="og:title"
+                    content={title ? title + ' – Fake Store API' : 'Fake Store API'}
+                />
+                <meta name="og:image" content={socialCard} />
+                <meta name="apple-mobile-web-app-title" content="Fake Store API" />
+                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+                <link rel="icon" href="/favicon.png" type="image/png" />
+            </>
+        )
     },
 }
 
